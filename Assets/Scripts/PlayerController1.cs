48b320ce8bdb53f8f174ef18dc289c96e61cfa7e
sartzu <saaraerving@Ohjelmointi-07-MacBook-Pro.local>
2021-11-16 10:35:16 +0200

    levelia 3 jnejne

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public AudioSource jumpsound;
+    public float jumpForce = 20;
+    public float gravityModifier;
+    //private bool timerIsRunning;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityModifier;
+        jumpsound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+
+    void LateUpdate()
+    {
+        //GameObject manager = GameObject.Find("SpawnManager");
+        //SpawnManager spawnManager = manager.GetComponent<SpawnManager>();
+
+        //timerIsRunning = spawnManager.timerIsRunning;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround /*&& timerIsRunning*/)
+        {
+            jumpsound.Play();
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+
+        }
+
+       }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+<div id="top"></div>
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+
+### Built With
+
+* [Unity](https://unity.com/)
+
+### Installation
+
+Clone the repo
+   ```sh
+   git clone https://github.com/varia-ict/varia-agile-20C-ryhma-2.git
+   ```
+
+[contributors-shield]: https://img.shields.io/github/contributors/varia-ict/varia-agile-20C-ryhma-2.svg?style=for-the-badge
+[contributors-url]: https://github.com/varia-ict/varia-agile-20C-ryhma-2/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/varia-ict/varia-agile-20C-ryhma-2.svg?style=for-the-badge
+[forks-url]: https://github.com/varia-ict/varia-agile-20C-ryhma-2/network/members
+[stars-shield]: https://img.shields.io/github/stars/varia-ict/varia-agile-20C-ryhma-2.svg?style=for-the-badge
+[stars-url]: https://github.com/varia-ict/varia-agile-20C-ryhma-2/stargazers
+[issues-shield]: https://img.shields.io/github/issues/varia-ict/varia-agile-20C-ryhma-2.svg?style=for-the-badge
+[issues-url]: https://github.com/varia-ict/varia-agile-20C-ryhma-2/issues
