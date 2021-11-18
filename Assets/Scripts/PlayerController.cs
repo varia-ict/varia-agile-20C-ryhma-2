1f116e159981f2356430bbbd46a310faac6fb3c4
sartzu <saaraerving@Ohjelmointi-07-MacBook-Pro.local>
2021-11-18 13:31:09 +0200

    saara tekee levelia 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRb;
+    //public GameObject playerJump;
     public AudioSource jumpsound;
     public float jumpForce = 20;
     public float gravityModifier;
-    private bool timerIsRunning;
     public bool isOnGround = true;
-
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         Physics.gravity *= gravityModifier;
         jumpsound = GetComponent<AudioSource>();
+        //playerJump = playerJump.GetComponent<BoxCollider2D>();
+        
     }
 
     // Update is called once per frame
 
     void LateUpdate()
     {
-        GameObject manager = GameObject.Find("SpawnManager");
-        SpawnManager spawnManager = manager.GetComponent<SpawnManager>();
+        //var isTouch = playerJump.GetComponent<BoxCollider2D>()
 
-        timerIsRunning = spawnManager.timerIsRunning;
-
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && timerIsRunning)
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             jumpsound.Play();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
@@ -37,10 +36,26 @@ public class PlayerController : MonoBehaviour
         }
 
        }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        isOnGround = true;
+    private void OnTriggerEnter2D(Collider2D collision)
+    { 
+        var obj = collision.gameObject;
+        Debug.Log("Collision: " + collision.gameObject + " Ground: " + obj.gameObject);
+        if (collision.gameObject == obj.gameObject)
+        {
+            Debug.Log("Jotain tapahtuu");
+            isOnGround = true;
+        }
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    //isOnGround = true;
+    //    var component = collision.gameObject.GetComponent<BoxCollider2D>();
+    //    Debug.Log(component);
+    //    if (component.CompareTag("Ground"))
+    //    {
+    //        isOnGround = true;
+    //    }
+
+    //}
 }
