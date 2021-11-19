1514ab337d325084de4734c86df2ce966d032329
Olavi Kurki <olavikurki@Ohjelmointi-16-MacBook-Pro.local>
2021-11-19 09:46:28 +0200

    skulailee

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,7 +11,7 @@ public class SpawnManager : MonoBehaviour
     public TextMeshProUGUI timeText;
     private Rigidbody2D playerRb;
 
-    private float timeRemaining = 5;
+    private float timeRemaining = 60;
     public bool timerIsRunning;
 
     // Start is called before the first frame update
