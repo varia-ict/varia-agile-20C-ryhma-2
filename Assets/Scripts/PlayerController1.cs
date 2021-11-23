1b98bd4da56b4e4f44d541d30064994dab40380a
Olavi Kurki <olavikurki@Ohjelmointi-16-MacBook-Pro.local>
2021-11-23 09:50:17 +0200

    menujosjoskus

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -1,73 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class PlayerController1 : MonoBehaviour
 {
     private Rigidbody2D playerRb;
-    //public GameObject playerJump;
     public AudioSource jumpsound;
-    public TextMeshProUGUI scoreText;
-
     public float jumpForce = 20;
     public float gravityModifier;
+    //private bool timerIsRunning;
     public bool isOnGround = true;
-    private int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         Physics.gravity *= gravityModifier;
         jumpsound = GetComponent<AudioSource>();
-        //playerJump = playerJump.GetComponent<BoxCollider2D>();
-        scoreText.gameObject.SetActive(true);
-        scoreText.text = "Score:" + score.ToString();
     }
 
     // Update is called once per frame
 
     void LateUpdate()
     {
-        //var isTouch = playerJump.GetComponent<BoxCollider2D>()
+        //GameObject manager = GameObject.Find("SpawnManager");
+        //SpawnManager spawnManager = manager.GetComponent<SpawnManager>();
 
+        //timerIsRunning = spawnManager.timerIsRunning;
 
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround /*&& timerIsRunning*/)
         {
             jumpsound.Play();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
-            Debug.Log("ilmassa");
+
         }
 
-    }
+       }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        //var obj = collision.gameObject;
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-            Debug.Log("osuu maahan");
-        }
-
-        if (collision.gameObject.CompareTag("Collectible"))
-        {
-            Destroy(collision.gameObject);
-            scoreText.text = "Score:" + score++.ToString();
-        }
+        isOnGround = true;
     }
-
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    //isOnGround = true;
-    //    var component = collision.gameObject.GetComponent<BoxCollider2D>();
-    //    Debug.Log(component);
-    //    if (component.CompareTag("Ground"))
-    //    {
-    //        isOnGround = true;
-    //    }
-
-    //}
 }
