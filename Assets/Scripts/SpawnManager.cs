9907c1109ee34ee28d65afb3d80ae99083e5d028
Rallijuoma <juusohuuskonen@Ohjelmointi-08-MacBook-Pro.local>
2021-11-30 12:06:18 +0200

    timer korjaus

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,10 @@ public class SpawnManager : MonoBehaviour
     public TextMeshProUGUI timeText;
     private Rigidbody2D playerRb;
     private float timeRemaining = 60;
-    private float timeRemaining2 = 150;
+    private float timeRemaining2 = 180;
+    private float timeRemaining3 = 60;
+    private float timeRemaining4 = 90;
+    private float timeRemaining5 = 60;
     public bool timerIsRunning;
 
     // Start is called before the first frame update
@@ -28,24 +31,24 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("2Dgame2"))
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2DGame"))
         {
             if (timerIsRunning)
                 if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-                timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining).ToString();
-            }
-            else
-            {
-                timeRemaining = 0;
-                timeText.gameObject.SetActive(false);
-                timerIsRunning = false;
+                {
+                    timeRemaining -= Time.deltaTime;
+                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining).ToString();
+                }
+                else
+                {
+                    timeRemaining = 0;
+                    timeText.gameObject.SetActive(false);
+                    timerIsRunning = false;
 
-                gameOver.SetActive(true);
-                playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                    gameOver.SetActive(true);
+                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
 
-            }
+                }
         }
         else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame2"))
         {
@@ -59,6 +62,71 @@ public class SpawnManager : MonoBehaviour
                 else
                 {
                     timeRemaining2 = 0;
+                    timeText.gameObject.SetActive(false);
+                    timerIsRunning = false;
+
+                    gameOver.SetActive(true);
+                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                }
+            }
+
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame3"))
+        {
+            if (timerIsRunning)
+            {
+                if (timeRemaining3 > 0)
+                {
+                    timeRemaining3 -= Time.deltaTime;
+                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining3).ToString();
+                }
+                else
+                {
+                    timeRemaining3 = 0;
+                    timeText.gameObject.SetActive(false);
+                    timerIsRunning = false;
+
+                    gameOver.SetActive(true);
+                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                }
+            }
+
+        }
+
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame4"))
+        {
+            if (timerIsRunning)
+            {
+                if (timeRemaining4 > 0)
+                {
+                    timeRemaining4 -= Time.deltaTime;
+                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining4).ToString();
+                }
+                else
+                {
+                    timeRemaining4 = 0;
+                    timeText.gameObject.SetActive(false);
+                    timerIsRunning = false;
+
+                    gameOver.SetActive(true);
+                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                }
+            }
+
+        }
+
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame5"))
+        {
+            if (timerIsRunning)
+            {
+                if (timeRemaining5 > 0)
+                {
+                    timeRemaining5 -= Time.deltaTime;
+                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining5).ToString();
+                }
+                else
+                {
+                    timeRemaining5 = 0;
                     timeText.gameObject.SetActive(false);
                     timerIsRunning = false;
 
