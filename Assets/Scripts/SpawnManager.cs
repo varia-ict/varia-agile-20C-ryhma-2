1bc84865d8514169b0309b44ef29cb558fbe2fc3
AleksiPamilo <aleksi.pamilo@live.com>
2021-11-30 13:06:13 +0200

    Respawn jos peli loppuu

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,16 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject gameOver;
     public TextMeshProUGUI timeText;
+    public TextMeshProUGUI gameOverTime;
     private Rigidbody2D playerRb;
     private float timeRemaining = 60;
-    private float timeRemaining2 = 180;
-    private float timeRemaining3 = 60;
-    private float timeRemaining4 = 90;
-    private float timeRemaining5 = 60;
+    private float timeRemaining2 = 5;//180;
+    private float timeRemaining3 = 5;//60;
+    private float timeRemaining4 = 5;//90;
+    private float timeRemaining5 = 5;//60;
+    private float restartTime = 7;
     public bool timerIsRunning;
+    private bool gameOverBool = false;
 
     // Start is called before the first frame update
     void Start()
@@ -31,110 +35,139 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2DGame"))
-        {
-            if (timerIsRunning)
-                if (timeRemaining > 0)
-                {
-                    timeRemaining -= Time.deltaTime;
-                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining).ToString();
-                }
-                else
-                {
-                    timeRemaining = 0;
-                    timeText.gameObject.SetActive(false);
-                    timerIsRunning = false;
 
-                    gameOver.SetActive(true);
-                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
-
-                }
-        }
-        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame2"))
+        if(gameOverBool == true)
         {
-            if (timerIsRunning)
+            gameOverTime.gameObject.SetActive(true);
+
+            if(restartTime > 0)
             {
-                if (timeRemaining2 > 0)
-                {
-                    timeRemaining2 -= Time.deltaTime;
-                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining2).ToString();
-                }
-                else
-                {
-                    timeRemaining2 = 0;
-                    timeText.gameObject.SetActive(false);
-                    timerIsRunning = false;
+                restartTime -= Time.deltaTime;
+                gameOverTime.text = "Respawning... " + Mathf.RoundToInt(restartTime).ToString();
+            }
+            else
+            {
+                gameOverBool = false;
+                gameOverTime.gameObject.SetActive(false);
+                SceneManager.LoadScene(sceneName: "2DGame");
+            }
+        }
+        else
+        {
+            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2DGame"))
+            {
+                if (timerIsRunning)
+                    if (timeRemaining > 0)
+                    {
+                        timeRemaining -= Time.deltaTime;
+                        timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining).ToString();
+                    }
+                    else
+                    {
+                        timeRemaining = 0;
+                        timeText.gameObject.SetActive(false);
+                        timerIsRunning = false;
+                        gameOverBool = true;
 
-                    gameOver.SetActive(true);
-                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                        gameOver.SetActive(true);
+                        playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+
+                    }
+            }
+            else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame2"))
+            {
+                if (timerIsRunning)
+                {
+                    if (timeRemaining2 > 0)
+                    {
+                        timeRemaining2 -= Time.deltaTime;
+                        timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining2).ToString();
+                    }
+                    else
+                    {
+                        timeRemaining2 = 0;
+                        timeText.gameObject.SetActive(false);
+                        timerIsRunning = false;
+                        gameOverBool = true;
+
+                        gameOver.SetActive(true);
+                        playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                    }
                 }
+
+            }
+            else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame3"))
+            {
+                if (timerIsRunning)
+                {
+                    if (timeRemaining3 > 0)
+                    {
+                        timeRemaining3 -= Time.deltaTime;
+                        timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining3).ToString();
+                    }
+                    else
+                    {
+                        timeRemaining3 = 0;
+                        timeText.gameObject.SetActive(false);
+                        timerIsRunning = false;
+                        gameOverBool = true;
+
+                        gameOver.SetActive(true);
+                        playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                    }
+                }
+
             }
 
-        }
-        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame3"))
-        {
-            if (timerIsRunning)
+            else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame4"))
             {
-                if (timeRemaining3 > 0)
+                if (timerIsRunning)
                 {
-                    timeRemaining3 -= Time.deltaTime;
-                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining3).ToString();
-                }
-                else
-                {
-                    timeRemaining3 = 0;
-                    timeText.gameObject.SetActive(false);
-                    timerIsRunning = false;
+                    if (timeRemaining4 > 0)
+                    {
+                        timeRemaining4 -= Time.deltaTime;
+                        timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining4).ToString();
+                    }
+                    else
+                    {
+                        timeRemaining4 = 0;
+                        timeText.gameObject.SetActive(false);
+                        timerIsRunning = false;
+                        gameOverBool = true;
 
-                    gameOver.SetActive(true);
-                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                        gameOver.SetActive(true);
+                        playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                    }
                 }
+
             }
 
-        }
-
-        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame4"))
-        {
-            if (timerIsRunning)
+            else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame5"))
             {
-                if (timeRemaining4 > 0)
+                if (timerIsRunning)
                 {
-                    timeRemaining4 -= Time.deltaTime;
-                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining4).ToString();
-                }
-                else
-                {
-                    timeRemaining4 = 0;
-                    timeText.gameObject.SetActive(false);
-                    timerIsRunning = false;
+                    if (timeRemaining5 > 0)
+                    {
+                        timeRemaining5 -= Time.deltaTime;
+                        timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining5).ToString();
+                    }
+                    else
+                    {
+                        timeRemaining5 = 0;
+                        timeText.gameObject.SetActive(false);
+                        timerIsRunning = false;
+                        gameOverBool = true;
 
-                    gameOver.SetActive(true);
-                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                        gameOver.SetActive(true);
+                        playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+
+
+                    }
                 }
+
             }
-
-        }
-
-        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("2Dgame5"))
-        {
-            if (timerIsRunning)
-            {
-                if (timeRemaining5 > 0)
-                {
-                    timeRemaining5 -= Time.deltaTime;
-                    timeText.text = "Time:" + Mathf.RoundToInt(timeRemaining5).ToString();
-                }
-                else
-                {
-                    timeRemaining5 = 0;
-                    timeText.gameObject.SetActive(false);
-                    timerIsRunning = false;
-
-                    gameOver.SetActive(true);
-                    playerRb.constraints = RigidbodyConstraints2D.FreezePositionX;
-                }
-            }
-
         }
     }
 }
+
+    
